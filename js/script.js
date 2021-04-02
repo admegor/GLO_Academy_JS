9d03f80a135dbe2dae087b448e7cb17ee59ed69c
admegor <admegor@mail.ru>
2021-04-02 15:54:29 +0500

    lesson 2nd easy

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-'use strict';
+let money = 100000,
+    income = 99000,
+    addExpenses = 'Проезд, Обед, Подписка, Обучение',
+    deposit = false,
+    mission = 15000000,
+    period = 12,
+    budgetDay = 4000;
+    
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log('addExpenses.length: ', addExpenses.length);
+console.log('Период равен ' + period + 'месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
 
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+addExpenses = addExpenses.toLowerCase().split(', ');
 
-alert('Hello world!');
-
-console.log('Hello world!');
+console.log('addExpenses.toLowerCase(): ', addExpenses);
+console.log('budgetDay: ', budgetDay);
