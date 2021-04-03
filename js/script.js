2431c4dc80c30efeda9a11374cc193807e6cc770
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2021-04-03 19:11:07 +0500

    lesson02 herd fix

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ let summ = arrNum[0];
 
 for (let i = 1; i <= arrNum.length - 1; i++) {
     summ *= Number(arrNum[i]);
-};
+}
 
 summ = summ ** 3;
 
+console.log('summ: ', summ);
+
 let arrSumm = String(summ).split('');
 
-console.log(arrSumm[0] + arrSumm[1]);
+console.log(String(summ).substr(0, 2));
