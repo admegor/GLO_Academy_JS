d158e53966441686ce39431562f31380e20c72a8
admegor <admegor@mail.ru>
2021-04-05 15:03:03 +0500

    lesson03
    Signed-off-by: admegor <admegor@mail.ru>

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-let money = 100000,
-    income = 99000,
-    addExpenses = 'Проезд, Обед, Подписка, Обучение',
-    deposit = false,
-    mission = 15000000,
+'use strict'
+const money = +prompt('Ваш месячный доход?'),
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    expenses1 = prompt('Введите обязательную статью расходов?'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+    expenses2 = prompt('Введите обязательную статью расходов?'),
+    amount2 = +prompt('Во сколько это обойдется?'),
+    income = 99000, //Доход из lesson01
+    mission = 1200000, //цель
     period = 12,
-    budgetDay = 4000;
+    budgetMonth = money - amount1 - amount2,
+    budgetDay = Math.floor(budgetMonth / 30);
     
 console.log(typeof(money));
 console.log(typeof(income));
@@ -12,8 +18,15 @@ console.log(typeof(deposit));
 console.log('addExpenses.length: ', addExpenses.length);
 console.log('Период равен ' + period + 'месяцев');
 console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
-
-addExpenses = addExpenses.toLowerCase().split(', ');
-
-console.log('addExpenses.toLowerCase(): ', addExpenses);
+console.log('addExpenses.toLowerCase(): ', addExpenses.toLowerCase().split(', '));
 console.log('budgetDay: ', budgetDay);
+console.log('budgetMonth: ', budgetMonth);
+console.log('budgetDay: ', budgetDay);
+console.log('Math.ceil(mission/budgetMonth): ', Math.ceil(mission/budgetMonth));
+
+let message = (budgetDay >= 1200) ? 'У вас высокий уровень дохода' : 
+(600 <= budgetDay) ? 'У вас средний уровень дохода' :
+(0 <= budgetDay) ? 'К сожалению у вас уровень дохода ниже среднего' :
+'Что-то пошло не так';
+
+console.log(message);
