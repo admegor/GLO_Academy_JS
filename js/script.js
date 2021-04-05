f81ac9480059760d975d7dc8ec579e52eacb9876
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2021-04-05 23:22:21 +0500

    lesson02_hard fix3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let num = 266219;
 let arrNum = String(num).split('');
 let summ = arrNum[0];
 
-for (let i = 1; i <= arrNum.length - 1; i++) {
+for (let i = 1; i < arrNum.length; i++) {
     summ *= Number(arrNum[i]);
 }
 
