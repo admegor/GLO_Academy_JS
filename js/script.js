58c684f17ee127739fb9febf05b32fcf5e50f906
admegor <admegor@mail.ru>
2021-04-08 11:35:57 +0500

    lesson03_hard

--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
 'use strict'
 
-const money = +prompt('Ваш месячный доход?'),
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    expenses1 = prompt('Введите обязательную статью расходов?'),
-    amount1 = +prompt('Во сколько это обойдется?'),
-    expenses2 = prompt('Введите обязательную статью расходов?'),
-    amount2 = +prompt('Во сколько это обойдется?'),
-    income = 'фриланс', //Доход из lesson01
-    mission = 1200000, //цель
-    period = 12,
-    budgetMonth = money - amount1 - amount2,
-    budgetDay = Math.floor(budgetMonth / 30);
+// Часть #1
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-console.log('addExpenses.length: ', addExpenses.length);
-console.log('Период равен ' + period + 'месяцев');
-console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
-console.log('addExpenses.toLowerCase(): ', addExpenses.toLowerCase().split(', '));
-console.log('budgetDay: ', budgetDay);
-console.log('budgetMonth: ', budgetMonth);
-console.log('budgetDay: ', budgetDay);
-console.log('Math.ceil(mission/budgetMonth): ', Math.ceil(mission / budgetMonth));
+let lang = prompt('Если вы прочитали эту надпись и поняли ее, введите ваш язык: ru \nIf you read this inscription and understood it, enter the language: en');
 
-if (Math.ceil(mission / budgetMonth) === Infinity) alert('Ошибка: ваш месячный доход ' + budgetMonth);
+const weekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+weekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-const message = (budgetDay >= 1200) ? 'У вас высокий уровень дохода' :
-    (600 <= budgetDay) ? 'У вас средний уровень дохода' :
-    (0 < budgetDay) ? 'К сожалению у вас уровень дохода ниже среднего' :
-    (budgetDay === 0) ? 'Ваш доход ' + budgetDay + ' рублей в мясяц, вы что-то делаете не так.' :
-    'Что-то пошло не так';
+if ( lang !== 'ru' && lang !== 'en') {
+    alert('Ошибка в программе: неверые данные.\nError: invalid data.');
+}
+
+if (lang === 'ru') console.log(weekRu);
+    else if (lang === 'en') console.log(weekEn);
+
+switch (lang) {
+    case 'ru':
+        console.log(weekRu);
+        break;
+    case 'en': 
+        console.log(weekEn);
+        break;
+}
+
+const langArr = {
+    'ru': weekRu,
+    'en': weekEn
+};
+
+console.log(langArr[lang]);
+
+// Часть #2
+
+let namePerson = prompt('Введите имя:');
+
+const message = (namePerson ==='Артем') ? 'директор' : 
+    (namePerson ==='Максим') ? 'преподаватель' : 
+     'студент';
 
 console.log(message);
