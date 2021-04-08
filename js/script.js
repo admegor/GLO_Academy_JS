49c40509774221238824d87059597e5d5a65bb81
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2021-04-08 20:55:24 +0500

    lesson04 fix

--- js/script.js
+++ js/script.js
@@ -1,19 +1,52 @@
 'use strict'
 
-const argument = prompt('Введите данные', '     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis vitae    ');
-let message;
+const money = +prompt('Ваш месячный доход?'),
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    expenses1 = prompt('Введите обязательную статью расходов?', 'Курсы GLO Academy'),
+    amount1 = +prompt('Во сколько это обойдется?', '1600'),
+    expenses2 = prompt('Введите обязательную статью расходов?', 'Поесть'),
+    amount2 = +prompt('Во сколько это обойдется?', '3000'),
+    income = 'фриланс', //Доход из lesson01
+    mission = 1200000, //цель
+    period = 12,
+    accumulatedMonth = getAccumulatedMonth(), // В переменной результат свободных средств в месяц после вычислений
+    budgetDay = Math.floor(accumulatedMonth / 30);
 
-function foo(argument) {
-    if (typeof argument !== 'string') {
-        message = 'Не строка';
-    } else if (argument.trim().length > 30) {
-        message = argument.trim().substr(0, 30) + '...';
-    } else {
-        message = argument;
-    }
+let getStatusIncome = function() {
+    const message = (budgetDay >= 1200) ? 'У вас высокий уровень дохода' :
+        (600 <= budgetDay) ? 'У вас средний уровень дохода' :
+        (0 < budgetDay) ? 'К сожалению у вас уровень дохода ниже среднего' :
+        (budgetDay === 0) ? 'Ваш доход ' + budgetDay + ' рублей в мясяц, вы что-то делаете не так.' :
+        'Что-то пошло не так';
+
     return message;
+};
+
+function showTypeOf(data) {
+    console.log(typeof(data));
 }
 
-foo(argument);
+// Сумма всех обязательных расходов в месяц
+function getExpensesMonth() {
+    return amount1 + amount2;
+}
 
-console.log(message);
+// Накопления за месяц - расходы
+function getAccumulatedMonth() {
+    return money - amount1 - amount2;
+}
+
+// За какой период будет достингута цель
+function getTargetMonth() {
+    return Math.ceil(mission / accumulatedMonth)
+}
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+console.log('getExpensesMonth: ', getExpensesMonth());
+console.log('addExpenses.toLowerCase(): ', addExpenses.toLowerCase().split(', '));
+console.log('getTargetMonth(): ', getTargetMonth());
+console.log('budgetDay: ', budgetDay);
+console.log('getStatusIncome(): ', getStatusIncome());
