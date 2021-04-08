d062ddd14769da588b6e406ec14bbdea373ed5ed
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2021-04-08 20:46:52 +0500

    lesson04_hard fix

--- js/script.js
+++ js/script.js
@@ -5,9 +5,8 @@ let message;
 
 function foo(argument) {
     if (typeof argument !== 'string') {
-        message = alert('Не строка');
-    }
-    else if (argument.trim().length > 30) {
+        message = 'Не строка';
+    } else if (argument.trim().length > 30) {
         message = argument.trim().substr(0, 30) + '...';
     } else {
         message = argument;
